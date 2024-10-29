33aed816c72e45c767530350cdd2df2afaeb3629
wediaklup <wediaklup@protonmail.ch>
2024-10-29 20:42:37 +0100

    add tables.sql

---
+++ tables.sql
@@ -0,0 +1,37 @@
+CREATE TABLE `users` (
+    id INT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    sha256 VARCHAR(64) NOT NULL,
+    validator VARCHAR(64) NOT NULL,
+    validation_time TIMESTAMP NOT NULL,
+    admin TINYINT(1)
+);
+
+CREATE TABLE `rooms` (
+    id INT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    creator INT NOT NULL,
+    FOREIGN KEY (creator) REFERENCES users (id)
+);
+
+CREATE TABLE `message` (
+    id INT PRIMARY KEY,
+    content VARCHAR(1024) NOT NULL,
+    tone VARCHAR(16),
+    authorid INT NOT NULL,
+    roomid INT NOT NULL,
+    sent DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    sound BLOB,
+    mime VARCHAR(64),
+    FOREIGN KEY (authorid) REFERENCES users (id)
+    FOREIGN KEY (roomid) REFERENCES rooms (id)
+);
+
+CREATE TABLE `room_link` (
+    roomid INT PRIMARY KEY,
+    userid INT PRIMARY KEY,
+    FOREIGN KEY (roomid) REFERENCES rooms (id)
+    FOREIGN KEY (userid) REFERENCES users (id)
+);
+
