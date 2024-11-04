9b7478489d11d387282e114e6eac0b170c5d2d69
miuboy <r_a_g@gmx.de>
2024-11-04 17:44:10 +0100

    wenn dus rückgängig machen willst mach, ich bin mir nich so sicher ob du das so willst

--- __init__.py
+++ __init__.py
@@ -1,11 +1,25 @@
-from flask import Flask, render_template, abort, jsonify, make_response
-
+from flask import Flask, render_template, abort, jsonify, make_response, session, redirect
+from functools import wraps
+from sqlite3 import connect
 
 app = Flask(__name__)
+con = connect("tables.sql")
+cur = con.cursor()
 
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
 
+@app.route("/login")
+def login():
+    return render_template("/login.html")
 
 @app.route("/")
+@login_required
 def root():
     return render_template("index.html")
 
