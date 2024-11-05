e434172235dfcc6a7c607f243cb41b403bb71634
miuboy <r_a_g@gmx.de>
2024-11-05 20:35:22 +0100

    loginlogik + seite

--- __init__.py
+++ __init__.py
@@ -1,10 +1,37 @@
-from flask import Flask, render_template, abort, jsonify, make_response, session, redirect
+from flask import Flask, render_template, abort, jsonify, make_response, request, redirect
 from functools import wraps
-from sqlite3 import connect
+from hashlib import sha256
+from datetime import datetime
+import psql
+from dbconnect import Adapter
 
 app = Flask(__name__)
-con = connect("tables.sql")
-cur = con.cursor()
+
+SERVERNAME = "hector"
+SCHEMA = "visual_messenger"
+
+class CommonSQLObject(psql.SQLObject):
+    SERVER_NAME = SERVERNAME
+    SCHEMA_NAME = SCHEMA
+
+class User(CommonSQLObject):
+    TABLE_NAME = "users"
+    SQL_KEYS = ["id", "name", "sha256", "validator", "validation_time", "admin"]
+    PRIMARY_KEY = "id"
+
+    def __init__(self, id:int, name:str, sha256:str, validator:str, validation_time:datetime, admin:bool):
+        super().__init__()
+        self.id = id
+        self.name = name
+        self.sha256 = sha256
+        self.validator = validator
+        self.validation_time = validation_time
+        self.admin = admin
+
+    @staticmethod
+    def construct(response):
+        return [User(x[0], x[1], x[2], x[3], x[4], x[5]) for x in response]
+
 
 def login_required(f):
     @wraps(f)
@@ -14,9 +41,20 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("/login.html")
+    if request.method == "GET":
+        return render_template("/login.html", error="")
+    elif request.method == "POST":
+        username = request.form["username"]
+        password = request.form["passsword"]
+
+        try:
+            users = User.get(name=username, sha256=sha256(password.encode("utf-8")).hexdigest())
+        except KeyError:
+            return render_template("login.html", error="invalid username/password")
+        
+        return redirect("/")
 
 @app.route("/")
 @login_required
